7e150531b59bb82561091c4ea7727c82496c9818
Ishar-Menon <isharmenon99@gmail.com>
2020-04-10 21:47:14 +0530

    Completed neo4j testing client

--- client.py
+++ client.py
@@ -1,27 +1,21 @@
+import sys
 from neo4j import GraphDatabase
 
+# Initiate connection to neo4j
 uri = "bolt://10.10.1.201:7687"
 driver = GraphDatabase.driver(uri, auth=("neo4j", "1234"))
 
+# Read queries 
+file = open("Queries.txt","r")
+data = file.read()
+queries = data.split("-------------------------------------");
+
+# Run query
 with driver.session() as session:
-    result = session.run("""MATCH (:Person {id:1129})<-[:HAS_CREATOR]-(m:Message)-[:REPLY_OF*0..]->(p:Post) MATCH (p)-[:HAS_CREATOR]->(c)RETURN                                                                                                            
-  m.id as messageId,                                                                                                
-  CASE exists(m.content)                                                                                            
-  WHEN true THEN m.content                                                                                          
-  ELSE m.imageFile                                                                                                  
-  END AS messageContent,
-  m.creationDate AS messageCreationDate,                                                                            
-  p.id AS originalPostId,                                                                                           
-  c.id AS originalPostAuthorId,
-  c.firstName as originalPostAuthorFirstName,                                                                       
-  c.lastName as originalPostAuthorLastName                                                                          
-  ORDER BY messageCreationDate DESC""")
+    result = session.run(queries[int(sys.argv[1]) - 1])
 
-print(result.records())
+# Print result
+# records = result.records()
 
-records = result.records()
-keys = result.keys()
-
-for i in records:
-    print(i)
-    print()
+# for i in records:
+#         print(i)

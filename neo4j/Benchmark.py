3f7a768e2fe31893616e70241036eb041c97f4c7
Siva Girish <sivagirish81@gmail.com>
2020-04-16 18:56:54 +0530

    Benchmark Results Updated

--- neo4j/Benchmark.py
+++ neo4j/Benchmark.py
@@ -17,7 +17,7 @@ for _ in range(NO_OF_TESTS):
     for i in range(NO_OF_TESTS_EACH):
         # No of tests
         print("Trial {}".format(i))
-        perf_cmd = 'sudo taskset -c 32-95 sudo perf stat -e task-clock,context-switches,cpu-migrations,page-faults,cycles,instructions,cache-references,cache-misses,branches,branch-misses,L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-dcache-store-misses,L1-dcache-prefetches,L1-dcache-prefetch-misses,L1-icache-loads,L1-icache-load-misses,L1-icache-prefetches,L1-icache-prefetch-misses -p 83328 -- sleep {} > Results/Test_{}/Trial_{}.txt'.format(time,_,i)
+        perf_cmd = 'sudo taskset -c 32-95 sudo perf stat -e task-clock,context-switches,cpu-migrations,page-faults,cycles,instructions,cache-references,cache-misses,branches,branch-misses,L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-dcache-store-misses,L1-dcache-prefetches,L1-dcache-prefetch-misses,L1-icache-loads,L1-icache-load-misses,L1-icache-prefetches,L1-icache-prefetch-misses -p 83328 -- sleep {} 2>Results/Test_{}/Trial_{}.txt'.format(time,_,i)
         client_cmd = 'sudo python client.py {}'.format(_ + 1)
         net_cmd = perf_cmd + ' && ' + client_cmd
         # perf && client for simultaneous execution

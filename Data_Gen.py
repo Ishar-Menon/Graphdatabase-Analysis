d6ec93ce53deda4bb7e830918f97b7ddbde2fedf
Siva Girish <sivagirish81@gmail.com>
2020-04-18 20:26:53 +0530

    Context - Switches Added

--- Data_Gen.py
+++ Data_Gen.py
@@ -1,55 +1,77 @@
 import csv
 import sys
 
+def AVG(Array,Trials):
+    Sum = 0
+    for i in Array:
+        Sum+=float(i)
+    return Sum/Trials
+
 def IC_Write(Instruction_count,NO_OF_TESTS):
     CSV_File = 'Instruction_Count.csv'
-    Headers = ['Query No.','Trial 1','Trial 2','Trial 3','Trial 4','Trial 5']
+    Headers = ['Query No.','Trial 1','Trial 2','Trial 3','Trial 4','Trial 5','Average']
     with open(CSV_File,'a+',newline='\n') as csv_file:
         csvwriter = csv.writer(csv_file)
         for _ in range(NO_OF_TESTS):
+            Instruction_count[_].append(AVG(Instruction_count[_],NO_OF_TESTS))
             Instruction_count[_].insert(0,_ + 1)
         csvwriter.writerow(Headers)
         csvwriter.writerows(Instruction_count)
 
 def IPC_Write(Ins_per_cycle,NO_OF_TESTS):
     CSV_File = 'Instruction_Per_Cycle.csv'
-    Headers = ['Query No.','Trial 1','Trial 2','Trial 3','Trial 4','Trial 5']
+    Headers = ['Query No.','Trial 1','Trial 2','Trial 3','Trial 4','Trial 5','Average']
     with open(CSV_File,'a+',newline='\n') as csv_file:
         csvwriter = csv.writer(csv_file)
         for _ in range(NO_OF_TESTS):
+            Ins_per_cycle[_].append(AVG(Ins_per_cycle[_],NO_OF_TESTS))
             Ins_per_cycle[_].insert(0,_ + 1)
         csvwriter.writerow(Headers)
         csvwriter.writerows(Ins_per_cycle)
 
 def CR_Write(Cache_References,NO_OF_TESTS):
     CSV_File = 'Cache_References.csv'
-    Headers = ['Query No.','Trial 1','Trial 2','Trial 3','Trial 4','Trial 5']
+    Headers = ['Query No.','Trial 1','Trial 2','Trial 3','Trial 4','Trial 5','Average']
     with open(CSV_File,'a+',newline='\n') as csv_file:
         csvwriter = csv.writer(csv_file)
         for _ in range(NO_OF_TESTS):
+            Cache_References[_].append(AVG(Cache_References[_],NO_OF_TESTS))
             Cache_References[_].insert(0,_ + 1)
         csvwriter.writerow(Headers)
         csvwriter.writerows(Cache_References)
 
 def LDL_Write(L1_dcache_loads,NO_OF_TESTS):
     CSV_File = 'L1_dcache_loads.csv'
-    Headers = ['Query No.','Trial 1','Trial 2','Trial 3','Trial 4','Trial 5']
+    Headers = ['Query No.','Trial 1','Trial 2','Trial 3','Trial 4','Trial 5','Average']
     with open(CSV_File,'a+',newline='\n') as csv_file:
         csvwriter = csv.writer(csv_file)
         for _ in range(NO_OF_TESTS):
+            L1_dcache_loads[_].append(AVG(L1_dcache_loads[_],NO_OF_TESTS))
             L1_dcache_loads[_].insert(0,_ + 1)
         csvwriter.writerow(Headers)
         csvwriter.writerows(L1_dcache_loads)
 
 def LDM_Write(L1_dcache_loads_misses,NO_OF_TESTS):
     CSV_File = 'L1_dcache_loads_misses.csv'
-    Headers = ['Query No.','Trial 1','Trial 2','Trial 3','Trial 4','Trial 5']
+    Headers = ['Query No.','Trial 1','Trial 2','Trial 3','Trial 4','Trial 5','Average']
     with open(CSV_File,'a+',newline='\n') as csv_file:
         csvwriter = csv.writer(csv_file)
         for _ in range(NO_OF_TESTS):
+            L1_dcache_loads_misses[_].append(AVG(L1_dcache_loads_misses[_],NO_OF_TESTS))
             L1_dcache_loads_misses[_].insert(0,_ + 1)
         csvwriter.writerow(Headers)
         csvwriter.writerows(L1_dcache_loads_misses)
+
+def CS_Write(Context_Switches,NO_OF_TESTS):
+    CSV_File = 'Context_Switches.csv'
+    Headers = ['Query No.','Trial 1','Trial 2','Trial 3','Trial 4','Trial 5','Average']
+    with open(CSV_File,'a+',newline='\n') as csv_file:
+        csvwriter = csv.writer(csv_file)
+        for _ in range(NO_OF_TESTS):
+            Context_Switches[_].append(AVG(Context_Switches[_],NO_OF_TESTS))
+            Context_Switches[_].insert(0,_ + 1)
+        csvwriter.writerow(Headers)
+        csvwriter.writerows(Context_Switches)
 
 NO_OF_TESTS = 5
 NO_OF_TESTS_EACH = 5
@@ -61,12 +83,16 @@ if (sys.argv[0] == 1):
     origin_path = origin_path_neo4j
 else:
     origin_path = origin_path_postgres
-
+origin_path = origin_path_neo4j
 IC = []
 IPC = []
 CR = []
 LDL = []
 LDM = []
+CS = []
+PF = []
+Cycles = []
+TS = []
 
 for query in range(NO_OF_TESTS):
     # Data Storage for each Test Case
@@ -75,6 +101,7 @@ for query in range(NO_OF_TESTS):
     Cache_References = []
     L1_dcache_loads = []
     L1_dcache_loads_misses =[]
+    Context_Switches = []
     for trial in range(NO_OF_TESTS_EACH):
         File_name = origin_path + 'Test_{}\\Trial_{}.txt'.format(query,trial)
         File = open(File_name,"r")
@@ -90,6 +117,8 @@ for query in range(NO_OF_TESTS):
         L1_dcache_loads.append(L1_dcache_load[0].replace(",",""))
         L1_dcache_loads_miss = '!'.join(Lines[14].strip().split()).split('!')
         L1_dcache_loads_misses.append(L1_dcache_loads_miss[0].replace(",",""))
+        Context_Switch = '!'.join(Lines[4].strip().split()).split('!')
+        Context_Switches.append(Context_Switch[0].replace(",",""))
     print(Instruction_count)
     IC.append(Instruction_count)
     print(Ins_per_cycle)
@@ -100,6 +129,8 @@ for query in range(NO_OF_TESTS):
     LDL.append(L1_dcache_loads)
     print(L1_dcache_loads_misses)
     LDM.append(L1_dcache_loads_misses)
+    print(Context_Switches)
+    CS.append(Context_Switches)
     print('__________________________________________________________________________________________________________________')
 #print(IC)
 
@@ -114,4 +145,4 @@ IPC_Write(IPC,5)
 CR_Write(CR,5)
 LDL_Write(LDL,5)
 LDM_Write(LDM,5)
-
+CS_Write(CS,5)

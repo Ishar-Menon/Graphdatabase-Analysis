9169767fb6e2f7c2510d2ad39fa471d9867d563f
Ishar-Menon <isharmenon99@gmail.com>
2020-04-21 20:10:22 +0530

    updated core values of shitProcess

--- ShiftProcess.py
+++ ShiftProcess.py
@@ -5,31 +5,31 @@
 import os
 # Retrieve all user's
 
-#Remove all pre-existing pid's
+# Remove all pre-existing pid's
 os.system('sudo rm usr_file')
 os.system('sudo rm upid_file')
 os.system('cut -d: -f1 /etc/passwd > usr_file')
-k = open("usr_file","r")
+k = open("usr_file", "r")
 udata = k.read()
 u_arr = udata.split()
 print(u_arr)
 
 # Retrieve pid's of processes run by all users and append them to upid_file
 for i in u_arr:
-        tmp = 'pgrep -u ' + i + '>>upid_file'
-        os.system(tmp)
+    tmp = 'pgrep -u ' + i + '>>upid_file'
+    os.system(tmp)
 print("\n")
-#print(get_root_pid)
-f = open("upid_file","r")
+# print(get_root_pid)
+f = open("upid_file", "r")
 data = f.read()
-#print(type(data))
+# print(type(data))
 data_arr = data.split("\n")
-#print(data_arr)
+# print(data_arr)
 
 # Taskset to shift all processes
 for i in data_arr:
-        if (i == ''):
-                break
-        temp = 'sudo taskset -cp 32-95 ' + i
-        print(temp)
-        os.system(temp)
+    if (i == ''):
+        break
+    temp = 'sudo taskset -cp 24-47,72-95 ' + i
+    print(temp)
+    os.system(temp)

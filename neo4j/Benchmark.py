02732834f4b9825b464be83b6b717f4765e3cd3b
Ishar-Menon <ishar1999messi@gmail.com>
2020-04-25 22:37:12 +0530

    Perf_Updated_Res

--- neo4j/Benchmark.py
+++ neo4j/Benchmark.py
@@ -1,16 +1,17 @@
+#!/usr/bin/env python
 import os
 import sys
 # run perf 5 times on each client file
 
-os.system('rm -rf Results')
-os.system('mkdir Results')
+os.system('rm -rf {}'.format(sys.argv[1]))
+os.system('mkdir {}'.format(sys.argv[1]))
 NO_OF_TESTS_EACH = 1
 NO_OF_TESTS = 5
 app_pid_file = open('app_pid','r')
 pid_list = app_pid_file.read()
 
 for _ in range(NO_OF_TESTS):
-    fname = 'sudo mkdir perf_curr_res/Test_{}'.format(_)
+    fname = 'sudo mkdir {}/Test_{}'.format(sys.argv[1],_)
     os.system(fname)
 time = 30
 
@@ -20,8 +21,8 @@ for _ in range(NO_OF_TESTS):
     for i in range(NO_OF_TESTS_EACH):
         # No of tests
         print("Trial {}".format(i))
-        perf_cmd = 'sudo taskset -a -c 24-47,72-95 sudo perf stat -e task-clock,context-switches,cpu-migrations,page-faults,cycles,instructions,cache-references,cache-misses,branches,branch-misses,L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-dcache-store-misses,L1-dcache-prefetches,L1-dcache-prefetch-misses,L1-icache-loads,L1-icache-load-misses,L1-icache-prefetches,L1-icache-prefetch-misses -p {} -- sleep {} 2>perf_new_res/Test_{}/Trial_{}.txt'.format(pid_list,
-            time,_, i)
+        perf_cmd = 'sudo taskset -a -c 24-47,72-95 sudo perf stat -e task-clock,context-switches,cpu-migrations,page-faults,cycles,instructions,cache-references,cache-misses,branches,branch-misses,L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-dcache-store-misses,L1-dcache-prefetches,L1-dcache-prefetch-misses,L1-icache-loads,L1-icache-load-misses,L1-icache-prefetches,L1-icache-prefetch-misses -p {} -- sleep {} 2>{}/Test_{}/Trial_{}.txt'.format(pid_list,
+            time,sys.argv[1],_, i)
         client_cmd = 'sudo taskset -a -c 24-47,72-95 python3 client.py {}'.format(_ + 1)
         net_cmd = perf_cmd + ' && ' + client_cmd
         # perf && client for simultaneous execution

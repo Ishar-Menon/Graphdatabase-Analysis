6c56ac6a1664b0941c1f09d4a1be2a0c2bc66952
Ishar-Menon <ishar1999messi@gmail.com>
2020-04-26 01:45:26 +0530

    PERF BENCH Updated

--- neo4j/Benchmark.py
+++ neo4j/Benchmark.py
@@ -5,7 +5,7 @@ import sys
 
 os.system('rm -rf {}'.format(sys.argv[1]))
 os.system('mkdir {}'.format(sys.argv[1]))
-NO_OF_TESTS_EACH = 1
+NO_OF_TESTS_EACH = 5
 NO_OF_TESTS = 5
 app_pid_file = open('app_pid','r')
 pid_list = app_pid_file.read()
@@ -15,6 +15,14 @@ for _ in range(NO_OF_TESTS):
     os.system(fname)
 time = 30
 
+#Empty_bench
+
+os.system('sudo mkdir {}/Empty_Bench'.format(sys.argv[1]))
+for _ in range(NO_OF_TESTS_EACH):
+    empty_cmd = 'sudo taskset -c 24-47,72-95 sudo perf stat -e task-clock,context-switches,cpu-migrations,page-faults,cycles,instructions,cache-references,cache-misses,branches,branch-misses,LLC-loads,LLC-load-misses,LLC-stores,LLC-prefetches,iTLB-loads,iTLB-load-misses,dTLB-loads,dTLB-load-misses,dTLB-prefetch-misses,L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-dcache-store-misses,L1-dcache-prefetches,L1-dcache-prefetch-misses,L1-icache-loads,L1-icache-load-misses,L1-icache-prefetches,L1-icache-prefetch-misses -C 0-11,48-59 -- sleep 30 2> {}/Empty_Bench/Empty_Bench_{}'.format(sys.argv[1],_)
+    print(empty_cmd)
+    os.system(empty_cmd)
+
 for _ in range(NO_OF_TESTS):
     # Benchmarks
     print("Test {} Results".format(_))
@@ -23,8 +31,8 @@ for _ in range(NO_OF_TESTS):
         print("Trial {}".format(i))
         #perf_cmd = 'sudo taskset -c 24-47,72-95 sudo perf stat -e task-clock,context-switches,cpu-migrations,page-faults,cycles,instructions,cache-references,cache-misses,branches,branch-misses,L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-dcache-store-misses,L1-dcache-prefetches,L1-dcache-prefetch-misses,L1-icache-loads,L1-icache-load-misses,L1-icache-prefetches,L1-icache-prefetch-misses -p {} -- sleep {} 2>{}/Test_{}/Trial_{}.txt'.format(pid_list,
         #    time,sys.argv[1],_, i)
-        perf_cores_cmd = 'sudo taskset -c 24-47,72-95 sudo perf stat -e task-clock,context-switches,cpu-migrations,page-faults,cycles,instructions,cache-references,cache-misses,branches,branch-misses,dTLB-loads,dTLB-load-misses,dTLB-prefetch-misses,L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-dcache-store-misses,L1-dcache-prefetches,L1-dcache-prefetch-misses,L1-icache-loads,L1-icache-load-misses,L1-icache-prefetches,L1-icache-prefetch-misses -C 0-11,48-59 -- sleep {} 2>{}/Test_{}/Trial_{}.txt'.format(time,sys.argv[1],_, i)
-        client_cmd = 'sudo taskset -c 24-47,72-95 python3 client.py {}'.format(_ + 1)
+        perf_cores_cmd = 'sudo taskset -c 24-47,72-95 sudo perf stat -e task-clock,context-switches,cpu-migrations,page-faults,cycles,instructions,cache-references,cache-misses,branches,branch-misses,LLC-loads,LLC-load-misses,LLC-stores,LLC-prefetches,iTLB-loads,iTLB-load-misses,dTLB-loads,dTLB-load-misses,dTLB-prefetch-misses,L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-dcache-store-misses,L1-dcache-prefetches,L1-dcache-prefetch-misses,L1-icache-loads,L1-icache-load-misses,L1-icache-prefetches,L1-icache-prefetch-misses -C 0-11,48-59 -- sleep {} 2>{}/Test_{}/Trial_{}.txt'.format(time,sys.argv[1],_, i)
+        client_cmd = 'python3 client.py {}'.format(_ + 1)
         net_cmd = perf_cores_cmd + ' && ' + client_cmd
         # perf && client for simultaneous execution
         print(net_cmd)
